170fdb7c30cfb61c0b658eff7df1c3f7ad55bc92
google.send2cny <google.send2cny@gmail.com>
2014-05-23 22:13:44 +0800

    1st Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#Prepare Accessors (i.e - set & get) for a Matrix object as well the inverse of itself
 makeCacheMatrix <- function(x = matrix()) {
-
+#vector i for inverse, just like m for mean in the example  
+  i <- NULL
+#Set for the Matrix
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+#Get for the Matrix
+  get <- function(){
+    x
+  }
+#Set for the inverse, just like the example setmean  
+  setinverse <- function(inverse){
+    i <<- inverse
+  }
+#Get for the inverse
+  getinverse <- function(){
+    i
+  }
+#Return value
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
-
-## Write a short comment describing this function
+#Just like the example, return the inverse if previous computed.
+#Else compute using solve() and return inverse. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Grap i from model (i.e - makeCacheMatrix)
+  i <- x$getinverse()
+# if previous the inverse of the matrix - (i) is computed, return value
+  if (!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+# Grap matrix from model  
+  data  <- x$get()
+# inverse the matrix
+  i  <- solve(data, ...)
+# pass the inverse to model
+  x$setinverse(i)
+# display the inverse
+  i
 }
+
